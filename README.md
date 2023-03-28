9c0a22ae351b88377e1dde8351c8bb43bfa7762b
Nagata Ichiko <106644839+nagata-ichiko@users.noreply.github.com>
2023-03-29 00:23:38 +0900

    GPU実装中

--- README.md
+++ README.md
@@ -6,14 +6,44 @@
 
 OpenAI が開発した文字起こし翻訳モデル Whisper と Dokcer と Gradio を使用した文字起こしアプリを作ります。
 
-# Codon
-
-実行コマンド
+# CPU を使用する場合
 
 ```
-codon run XXXX.py
+docker-compose up -d
 ```
 
+# GPU を使用する場合
+
+## NVIDIA CUDA Toolkit のインストール
+
 ```
-codon run -release XXX.py
+https://developer.nvidia.com/cuda-toolkit-archive
+```
+
+## イメージ選択
+
+マシンの GPU に合わせた Image を選択し、Dockerfile を更新する。
+
+Image は以下
+
+```
+https://hub.docker.com/r/pytorch/pytorch/tags?page=1
+```
+
+Driver については以下やググった結果を参考にする
+
+```
+https://pystyle.info/pytorch-relationship-between-gpu-and-driver-cuda-and-cudnn-versions/
+```
+
+## docker-compose.yml 書き換え
+
+GPU 側のコメントアウトを解除
+
+CPU 側をコメントアウト
+
+## 実行
+
+```
+docker-compose up -d
 ```
---
+++ src/WhisperGradioApp_GPU.py
@@ -0,0 +1,51 @@
+from multiprocessing.sharedctypes import Value
+import whisper
+from faster_whisper import WhisperModel
+import gradio as gr 
+model = "large-v2"
+from datetime import timedelta
+from srt import Subtitle
+import srt
+
+model = WhisperModel(model, device="cuda", compute_type="flote16")
+
+def speechRecognitionModel(input): 
+    segments, _ = model.transcribe(input,language="ja", beam_size=2, word_timestamps=False)    
+    result =""
+    out_text = []
+    # segment情報から発言の開始/終了時間とテキストを抜き出し、srt形式で編集する
+    for segment in segments:
+        print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+        result += "[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text)
+        result += "\n"
+        start = segment.start
+        end = segment.end
+        text = segment.text
+        out_line = Subtitle(index=1,\
+                            start=timedelta(seconds=timedelta(seconds=start).seconds,\
+                            microseconds=timedelta(seconds=start).microseconds),\
+                            end=timedelta(seconds=timedelta(seconds=end).seconds,\
+                            microseconds=timedelta(seconds=end).microseconds),\
+                            content=text,\
+                            proprietary='')
+        out_text.append(out_line)
+    with open("src/sample" + ".csv", mode="w", encoding="utf-8") as f:
+        origin = srt.compose(out_text)
+        origin = origin.replace(",",".")
+        origin = origin.replace("\n",",")
+        origin = origin.replace(",,","\n")
+        f.write(origin)
+    
+    return result
+
+gr.Interface(
+    title = 'Whisper Sample App', 
+    fn=speechRecognitionModel, 
+    inputs=[
+        # 動画ファイル
+        gr.Video(type="filepath")
+    ],
+    outputs=[
+        "textbox"
+    ],
+    live=True).launch(server_name = "0.0.0.0", server_port=8070, share=True)
