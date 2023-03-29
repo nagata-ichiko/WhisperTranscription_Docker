14f4af763a7f746b09be4694b55d40d4ad9e095a
Nagata Ichiko <106644839+nagata-ichiko@users.noreply.github.com>
2023-03-30 01:36:40 +0900

    Revert "作業保存"
    This reverts commit 51bdf691beb0b03c0f11d06bb36b605911d5010d.

--- docker.sh
+++ docker.sh
@@ -3,18 +3,15 @@ docker exec -it whisper-gpu bash -c "
 
     # コマンドを実行する
     # pip freeze > requirements_conda.txt
-    conda update conda
     conda init
+    echo 'conda activate py39_env' >> ~/.bashrc
     conda config --set env_prompt '({name})'
     conda config --set default_env py39_env
     conda init
-  "
-
-docker exec -it whisper-gpu bash -c "
-    # conda install --name py39_env --file requirements_conda.txt
-    pip install --upgrade pip --user
-    pip install -r requirements.txt  --user
-    pip install ctranslate2 OpenNMT-py sentencepiece  --user
-    pip install transformers --user
+    pip install --upgrade pip
+    pip install -r requirements_conda.txt
+    pip install -r requirements.txt
+    pip install ctranslate2 OpenNMT-py sentencepiece
+    pip install transformers
     python src/WhisperGradioApp_CPU.py
 "
